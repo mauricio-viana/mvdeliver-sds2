19f7d701cb3ce0f4ccf365526f30a072247ea6f7
Mauricio Viana <mau_viana@live.com>
2021-01-05 16:47:12 -0300

    Project Created

---
+++ backend/src/main/java/com/mauricioviana/mvdeliver/MvdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.mauricioviana.mvdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvdeliverApplication.class, args);
+	}
+
+}
